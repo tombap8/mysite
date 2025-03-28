63bb674a9a24cfeb14a3a494866ba430cc4048a9
Tom B. Parker <tombap8@hotmail.com>
2025-03-28 12:54:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php 첫페이지</title>
+    <script>
+        // 보그 페이지로 바로 가기!
+        location.replace('/vogue/');
+    </script>
 </head>
 <body>
     <h1>나의 php 첫 페이지야!ㅎㅎㅎ</h1>
