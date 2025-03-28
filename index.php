146f7fbabe3ed460730809226db06d00c428774d
Tom B. Parker <tombap8@hotmail.com>
2025-03-28 12:04:34 +0900

    ss

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php 첫페이지</title>
+</head>
+<body>
+    <h1>나의 php 첫 페이지야!</h1>
+    
+</body>
+</html>
